d0762673874dfde968a1ef4450bfa4724b529d96
Muhammad Ahsan <ahsan.moin@indusvalley.io>
2023-01-07 11:54:44 +0500

    second

--- src/core/decorators/get-current-user-id.decorator.ts
+++ src/core/decorators/get-current-user-id.decorator.ts
@@ -7,7 +7,7 @@ export const GetCurrentUserId = createParamDecorator(
     const request = context.switchToHttp().getRequest();
     const user = request.user as JwtPayload;
     return user.sub; // For Getting User Id
-  },
+  },     
 );
 
 // export const GetCurrentLawyerId = createParamDecorator(
